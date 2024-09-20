670e292a41b000075603cc298aafaf7a28a6bc15
Godfrey Mutwol <mutwolgodfrey@gmail.com.com>
2024-09-19 18:16:14 -0700

    unittests_i

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -1,19 +1,14 @@
-function calculateNumber(type, a, b) {
-  const aRound = Math.round(a);
-  const bRound = Math.round(b);
-  
+const calculateNumber = (type, a, b) => {
   if (type === 'SUM') {
-    return aRound + bRound;
+    return Math.round(a) + Math.round(b);
   }
   if (type === 'SUBTRACT') {
-    return aRound - bRound;
+    return Math.round(a) - Math.round(b);
   }
   if (type === 'DIVIDE') {
-    if (bRound === 0) {
-      return 'Error';
-    }
-    return aRound / bRound;
+    return Math.round(b) === 0 ? 'Error' : Math.round(a) / Math.round(b);
   }
-}
+  return 0;
+};
 
 module.exports = calculateNumber;
--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,20 +1,14 @@
-{
-  "name": "8-api",
-  "version": "1.0.0",
-  "description": "",
-  "main": "index.js",
-  "scripts": {
-    "test": "./node_modules/mocha/bin/mocha"
-  },
-  "author": "",
-  "license": "ISC",
-  "dependencies": {
-    "express": "^4.17.1"
-  },
-  "devDependencies": {
-    "chai": "^4.2.0",
-    "mocha": "^6.2.2",
-    "request": "^2.88.0",
-    "sinon": "^7.5.0"
-  }
-}
+const request = require('request');
+const { expect } = require('chai');
+
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
+
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(body).to.be.equal('Welcome to the payment system');
+      done();
+    });
+  });
+});
